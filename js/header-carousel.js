fa5731cd595d748f87e9ce5bc27f30461a8ba542
deadpanda1 <esberntk@gmail.com>
2017-09-04 00:10:41 +0200

    fixed white fade, now fades between images

--- js/header-carousel.js
+++ js/header-carousel.js
@@ -4,17 +4,20 @@ var carouselImages = ["images/big/Ezy6482.jpg",
                       "images/big/EZYPRO_0075v3.jpg"
 					 ];
 var caroIndex = 0;
+var loadedImages = new Array();
 $(document).ready(function() {
+	
+	/*for(var i = 0; i < carouselImages.length; i++){
+		loadedImages[i] = new Image();
+		loadedImages[i].src = carouselImages[i];
+	}*/
 	var carousel = function(){
-		
-		$('header').fadeOut(1000,function(){
 			$('header')
 				.css('background-image','url("'+ carouselImages[caroIndex] + '")')
-				.fadeIn(500);
-		});
+				.fadeIn(5000);
 		caroIndex++;
 		caroIndex = caroIndex % carouselImages.length;
-		setTimeout(carousel,7000);
+		setTimeout(carousel,10000);
 	};
 	carousel();
 });
